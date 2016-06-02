6393bce4368e3a2e30e4ac43fea6e27de57dd227
Daniel Christopher <dan_pc@live.com>
2016-06-02 19:17:45 -0400

    Switched to view pager for main navigation

--- friendly/FindMovesViewController.swift
+++ friendly/FindMovesViewController.swift
@@ -8,43 +8,15 @@
 
 import UIKit
 
-class FindMovesViewController: UIViewController {
-    var pageMenu : CAPSPageMenu?
+class FindMovesViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Array to keep track of controllers in page menu
-        var controllerArray : [UIViewController] = []
-        
-        let friendsController = storyboard!.instantiateViewControllerWithIdentifier("friends_moves")
-        friendsController.title = "Friends"
-        
-        let areaController = storyboard!.instantiateViewControllerWithIdentifier("area_moves")
-        areaController.title = "Area"
-        
-        controllerArray.append(friendsController);
-        controllerArray.append(areaController)
-        
-        let primaryBlue = UIColor(red: 0.13, green: 0.59, blue: 0.95, alpha: 1);
-        
-        let parameters: [CAPSPageMenuOption] = [
-            .MenuItemSeparatorWidth(4.3),
-            .UseMenuLikeSegmentedControl(true),
-            .MenuItemSeparatorPercentageHeight(0.1),
-            .ScrollMenuBackgroundColor(primaryBlue),
-            .SelectionIndicatorColor(UIColor(red: 1, green: 0.25, blue: 0.50, alpha: 1)),
-            .MenuItemSeparatorColor(primaryBlue),
-            .UnselectedMenuItemLabelColor(UIColor.darkGrayColor())
-        ]
-        
-        let navheight = (navigationController?.navigationBar.frame.size.height ?? 0) + UIApplication.sharedApplication().statusBarFrame.size.height
-        
-        let frame = CGRectMake(0, navheight, view.frame.width, view.frame.height-navheight)
-        
-        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: frame, pageMenuOptions: parameters)
-        
-        self.view.addSubview(pageMenu!.view)
+        let navigationBar = self.navigationController?.navigationBar
+        navigationBar?.barTintColor = UIColor(red: 0.13, green: 0.59, blue: 0.95, alpha: 1)
+        navigationBar?.topItem?.title = "Open Moves"
+        navigationBar?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
     }
     
     override func didReceiveMemoryWarning() {
---
+++ friendly/MainNavigationViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MainNavigationViewController.swift
+//  friendly
+//
+//  Created by Daniel Christopher on 6/2/16.
+//  Copyright © 2016 Daniel Christopher. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    private(set) lazy var pages: [UIViewController] = {
+        return [self.createPage("find_moves"),
+                self.createPage("make_moves"),
+                self.createPage("moves")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        dataSource = self
+        
+        if let firstViewController = pages.last {
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let numberOfPages = pages.count
+        
+        guard numberOfPages > nextIndex else {
+            return nil
+        }
+        
+        return pages[nextIndex]
+    }
+    
+    private func createPage(storyboardId: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle:nil).instantiateViewControllerWithIdentifier(storyboardId)
+    }
+}
+
