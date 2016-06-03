c948ce7b66624cdef9ecc74c9817650dce3f186b
Daniel Christopher <dan_pc@live.com>
2016-06-02 21:58:02 -0400

    Added dialog for selecting time

--- friendly/CreateHangoutOrMeetPeopleViewController.swift
+++ friendly/CreateHangoutOrMeetPeopleViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class CreateHangoutOrMeetPeopleViewController: UIViewController {
     
+    @IBOutlet weak var messageTextField: KaedeTextField!
+
+    @IBOutlet weak var addTimeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onAddTimeClicked(sender: UIButton) {
+        let width = self.view.frame.width - 25
+        let appearance = SCLAlertView.SCLAppearance(kWindowWidth: width)
+        
+        let alert = SCLAlertView(appearance: appearance)
+        
+        let datePicker = UIDatePicker()
+        datePicker.transform = CGAffineTransformMakeScale(0.8, 0.8);
+        datePicker.frame = CGRect(x: 0, y: 0, width: width, height: 300)
+        
+        alert.customSubview = datePicker
+     
+        alert.showEdit(
+            "When?",
+            subTitle: "",
+            colorStyle: 0x2196F3,
+            colorTextButton: 0xFFFFFF
+        )
+    }
 }
