9a1ebd12b56e84209d4c6305331d552882b5d4bf
Daniel Christopher <dan_pc@live.com>
2016-06-03 23:48:25 -0400

    Added swipe refresh

--- friendly/AcceptedMovesViewController.swift
+++ friendly/AcceptedMovesViewController.swift
@@ -9,31 +9,39 @@
 import UIKit
 
 class AcceptedMovesViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
         let primary = UIColor(red: 0.13, green: 0.59, blue: 0.95, alpha: 1)
         let navigationBar = self.navigationController?.navigationBar
         navigationBar?.barTintColor = primary
         navigationBar?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         navigationBar?.topItem?.title = "My Moves"
+        
+        setUpPullToRefresh(UIColor(red: 0.10, green: 0.46, blue: 0.82, alpha: 1))
+    }
+    
+    deinit {
+        tableView.dg_removePullToRefresh()
+    }
+    
+    private func setUpPullToRefresh(color: UIColor) {
+        let loadingView = DGElasticPullToRefreshLoadingViewCircle()
+        loadingView.tintColor = UIColor.whiteColor()
+        tableView.dg_addPullToRefreshWithActionHandler({ [weak self] () -> Void in
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
+                self?.tableView.dg_stopLoading()
+            })
+            }, loadingView: loadingView)
+        tableView.dg_setPullToRefreshFillColor(color)
+        tableView.dg_setPullToRefreshBackgroundColor(tableView.backgroundColor!)
+    }
+    
+    func refresh(sender:AnyObject) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- friendly/MovesViewController.swift
+++ friendly/MovesViewController.swift
@@ -9,35 +9,22 @@
 import UIKit
 
 class MovesViewController: UIViewController {
-    var pageMenu : CAPSPageMenu?
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
     
+    override func loadView() {
+        super.loadView()
+        
         let myMovesController = storyboard!.instantiateViewControllerWithIdentifier("accepted_moves")
+        
         myMovesController.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
         myMovesController.title = "Your Moves"
-
+        
         self.addChildViewController(myMovesController)
         self.view.addSubview(myMovesController.view)
         myMovesController.didMoveToParentViewController(self)
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
-
 }
