0c82282d5e35e1881ced303058baa2ed0213e1ce
Daniel Christopher <dan_pc@live.com>
2016-06-12 19:06:32 -0400

    Linked up view controllers for managing your moves

---
+++ moves/WhoIsDownViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  WhoIsDownViewModel.swift
+//  moves
+//
+//  Created by Daniel Christopher on 6/12/16.
+//  Copyright © 2016 Daniel Christopher. All rights reserved.
+//
+
+class WhoIsDownViewModel {
+    private var moveService: MoveService
+    private var userService: UserService
+    
+    init(moveService: MoveService, userService: UserService) {
+        self.moveService = moveService
+        self.userService = userService
+    }
+    
+    func acceptUser() {
+        
+    }
+    
+    func declineUser() {
+        
+    }
+    
+    func getPeopleWhoAreDown() {
+        
+    }
+}
